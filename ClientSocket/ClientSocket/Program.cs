dc9326aacd450f187b06b95b58a4f7668bf53d3b
ZadikHaGadol <145447470+ZadikHaGadol@users.noreply.github.com>
2023-09-19 13:40:11 +0300

    Add files via upload

---
+++ ClientSocket/ClientSocket/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientSocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int BUFFER_SIZE = 2048;
+            IPAddress server_ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipe = new IPEndPoint(server_ip, 1234);
+            Socket cs = new Socket(server_ip.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+
+            cs.Connect(ipe);
+
+            string msg;
+            byte[] b = new byte[BUFFER_SIZE];
+
+            //abcdefgh
+            //BB
+            //BBcdefgh
+
+            Array.Clear(b, 0, b.Length);
+            cs.Receive(b);
+
+            msg = Encoding.ASCII.GetString(b).TrimEnd('\0');
+            string result;
+            while (msg != "quit")
+            {
+                Console.WriteLine("[+] Received from server: {0}", msg);
+
+                result = msg;
+
+                cs.Send(Encoding.ASCII.GetBytes(result));
+
+                Array.Clear(b, 0, b.Length);
+                cs.Receive(b);
+                msg = Encoding.ASCII.GetString(b).TrimEnd('\0');
+            }
+            cs.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ServerSocket
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //IPHostEntry ipe = Dns.GetHostEntry("localhost");
+            //IPAddress[] ips = ipe.AddressList;
+
+            //foreach (IPAddress ip in ips)
+            //{
+            //    Console.WriteLine(ip.ToString());
+            //}
+
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Any, 1234);
+            Socket server_socket = new Socket(IPAddress.Any.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            server_socket.Bind(ipe);
+            server_socket.Listen(5);
+
+            server_socket.Accept();
+
+            Socket client_socket = server_socket.Accept();
+            Console.WriteLine("[+] Connection from: {0}", client_socket.RemoteEndPoint);
+
+            Console.WriteLine("Enter message to send: ");
+            string msg;
+            msg = Console.ReadLine();
+
+            while (msg != "quit")
+
+            {
+                client_socket.Send(Encoding.ASCII.GetBytes(msg));
+
+                byte[] sb = new byte[2048];
+
+                Array.Clear(sb, 0, sb.Length);
+                client_socket.Receive(sb);
+
+                Console.WriteLine(Encoding.ASCII.GetString(sb).TrimEnd('\0'));
+
+                Console.WriteLine("Enter message to send: ");
+                msg = Console.ReadLine();
+
+            }
+            
+            
+            client_socket.Close();
+                server_socket.Close();
+
+
+                Console.ReadKey();
+            
+        }
+    }
+}
